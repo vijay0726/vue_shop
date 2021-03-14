df8e5329171973d4290028238c7bf69c9ac87a1a
王杰 <2623312985@qq.com>
2021-03-14 18:01:38 +0800

    完成登录功能

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -44,8 +44,8 @@ export default {
     return {
       //这是登录表单的数据绑定对象
       loginForm: {
-        username: "zs",
-        password: "123",
+        username: "admin",
+        password: "123456",
       },
       //这是表单的验证规则
       loginFormRules: {
@@ -75,8 +75,19 @@ export default {
     },
     //点击登录,进行表单验证
     login() {
-      this.$refs.loginFormRef.validate((valid) => {
-        console.log(valid);
+      this.$refs.loginFormRef.validate(async (valid) => {
+        // console.log(valid);
+        if (!valid) return;
+        const { data: res } = await this.$http.post("login", this.loginForm);
+        if (res.meta.status !== 200) return this.$message.error("登陆失败！");
+        this.$message.success("登录成功！");
+        //1.将登陆成功之后的token,保存到客户端的 sessionStorage中
+        //  1.1项目中除了登陆之外的其他API接口，必须在登陆之后才能访问
+        //  1.2token 只应在当前网站打开期间生效，所以将token 保存在 sessionStorage 中
+        console.log(res);
+        window.sessionStorage.setItem("token", res.data.token);
+        //2. 通过编程式导航跳转到后台主页，路由地址是 /home
+        this.$router.push("/home");
       });
     },
   },
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,31 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
+import Home from '@/components/Home'
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/', redirect: '/login' },
-  { path: '/login', component: Login }
+  { path: '/login', component: Login },
+  { path: '/home', component: Home }
 ]
 
 const router = new VueRouter({
   routes
 })
 
+//挂在路由导航守卫
+router.beforeEach((to, from, next) => {
+  //to将要访问的路径
+  //from 代表从哪个路径跳转而来
+  //next 是一个函数，表示放行
+  //  next() 放行  next('/login')强制跳转
+
+  if (to.path === '/login') return next()
+  //获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
+})
 export default router
